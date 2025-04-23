b1e454ca9db19e4cf20a3732e60d3ca4b633fde8
JunhaDex <dk.junha.dev@gmail.com>
2025-04-24 00:04:00 +0900

    vue shadcn

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vue Dashboard Template with shadcn UI
+
+## Tech Stack
+
+### Frontend
+- Vue 3
+  - Vue Router
+  - Pinia Store
+  - Vue Use
+
+### Utils
+- Axios
+- Dayjs
+
+### Style
+- Tailwind CSS
+- PostCSS
+- shadcn UI
+
+### Build Tool
+- Vite
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1 class="text-xl">Foo</h1>
+    <Button size="sm" variant="secondary" disabled>Medium</Button>
+    <Button size="lg" @click="handleClick">Large</Button>
+  </main>
+</template>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button.ts'
+
+function handleClick() {
+  console.log('Button clicked!')
+}
+</script>
